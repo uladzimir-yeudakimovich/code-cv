e6fc0b82607c9c341d1d3979dd4ae9be2462e6fc
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2022-11-11 19:26:26 +0100

    code refactor

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { TokenStorageService } from './token-storage.service';
@@ -8,7 +8,7 @@ export class AuthGuard implements CanActivate {
 
     constructor(private tokenStorageService: TokenStorageService, private router: Router) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    canActivate(route: ActivatedRouteSnapshot): Observable<boolean> | Promise<boolean> | boolean {
         if (!!this.tokenStorageService.getToken()) {
             return true;
         } else {
