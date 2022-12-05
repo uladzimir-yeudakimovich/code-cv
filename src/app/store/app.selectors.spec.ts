26d27776836b5b3bae949f25f9ef7ed99b98218a
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2022-12-05 21:58:20 +0100

    update select test

--- src/app/store/app.selectors.spec.ts
+++ src/app/store/app.selectors.spec.ts
@@ -17,5 +17,38 @@ describe('app selectors', () => {
 
             expect(selectors.selectInformation.projector(appState)).toEqual(mockInformation);
         });
+
+        it('should get in progress status if action type in progress', () => {
+            const actionTypesInprogress = ['Load data', 'Update data'];
+            const changeIndicatorState = {
+                inProgress: actionTypesInprogress,
+                successful: [],
+                failed: [],
+            };
+
+            expect(selectors.selectInformationLoadInProgress(actionTypesInprogress).projector(changeIndicatorState)).toBe(true);
+        });
+
+        it('should get in progress status if action type success', () => {
+            const actionTypesSuccess = ['Load data success', 'Update data success'];
+            const changeIndicatorState = {
+                inProgress: [],
+                successful: actionTypesSuccess,
+                failed: [],
+            };
+
+            expect(selectors.selectInformationLoadSuccess(actionTypesSuccess).projector(changeIndicatorState)).toBe(true);
+        });
+
+        it('should get in progress status if action type failed', () => {
+            const actionTypesFailed = ['Load data failed', 'Update data failed'];
+            const changeIndicatorState = {
+                inProgress: [],
+                successful: [],
+                failed: actionTypesFailed,
+            };
+
+            expect(selectors.selectInformationLoadFailed(actionTypesFailed).projector(changeIndicatorState)).toBe(true);
+        });
     });
 });
--- src/app/store/app.selectors.ts
+++ src/app/store/app.selectors.ts
@@ -1,6 +1,20 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
+import { some, includes } from 'lodash';
 import { AppState } from './app.reducer';
 
 export const selectAppState = createFeatureSelector<AppState>('app');
 export const selectInformation = createSelector(selectAppState, state => state.generalInfo);
-export const selectInformationLoadInProgress = createSelector(selectAppState, state => state.changeIndicator);
+
+const selectChangeIndicatorState = createSelector(selectAppState, state => state.changeIndicator);
+export const selectInformationLoadInProgress = (actionTypes: string[]) => createSelector(
+    selectChangeIndicatorState,
+    state => some(actionTypes, (actionType) => includes(state.inProgress, actionType)),
+);
+export const selectInformationLoadSuccess = (actionTypes: string[]) => createSelector(
+    selectChangeIndicatorState,
+    state => some(actionTypes, (actionType) => includes(state.successful, actionType)),
+);
+export const selectInformationLoadFailed = (actionTypes: string[]) => createSelector(
+    selectChangeIndicatorState,
+    state => some(actionTypes, (actionType) => includes(state.failed, actionType)),
+);
